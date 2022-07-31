b3a53c47d261eef9fe7d39fc4806d9fa9c48f703
Piotr Perczak <wojownik102pl@Wp.pl>
2022-07-31 11:02:48 +0200

    NUnit

---
+++ NUnit/PrimeService.Tests/AnimalService_IsSoundEqual.cs
@@ -0,0 +1,54 @@
+﻿#region Sample_FirstTest
+using NUnit.Framework;
+using Prime.Services;
+using System;
+
+namespace Prime.UnitTests.Services
+{
+    //TEST Animal class
+    [TestFixture]
+    public class AnimalService_IsSoundEqual
+    {
+        private Cat _cat;
+        private Dog _dog;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _cat = new Cat();
+            _dog = new Dog();
+        }
+
+        [Test]
+        public void Cat_MakeSounds_ReturnEqual()
+        {
+            var result = _cat.MakeSound();
+
+            Assert.AreEqual(result, "meow meow");
+        }
+
+        [Test]
+        public void Cat_MakeSounds_ReturnNotEqual()
+        {
+            var result = _cat.MakeSound();
+
+            Assert.AreNotEqual(result, "moo moo");
+        }
+
+        [Test]
+        public void Dog_MakeSounds_ReturnEqual()
+        {
+            var result = _dog.MakeSound();
+
+            Assert.AreEqual(result, "woof woof");
+        }
+
+        [Test]
+        public void Dog_MakeSounds_ReturnNotEqual()
+        {
+            var result = _dog.MakeSound();
+            Assert.AreNotEqual(result, "moo moo");
+        }
+        #endregion
+    }
+}
---
+++ NUnit/PrimeService/AnimalService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public abstract class Animal
+    {
+        public virtual string MakeSound()
+        {
+            Console.WriteLine("Animal gives a sound!");
+            return "...";
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override string MakeSound()
+        {
+            Console.WriteLine("Dog sounds like: woof woof !");
+            return "woof woof";
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override string MakeSound()
+        {
+            Console.WriteLine("Cat says: meow meow !");
+            return "meow meow";
+        }
+    }
+}
